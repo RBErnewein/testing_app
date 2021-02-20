0c594fd2955eb5bb452e4f20e06fe59792be42f8
Rob Ernewein <54925187+RBErnewein@users.noreply.github.com>
2021-02-19 21:22:18 -0600

    Step 5-Test 1
    Step 5: Test 1 complete

--- test/models/favorites_test.dart
+++ test/models/favorites_test.dart
@@ -10,5 +10,12 @@ void main() {
       favorites.add(number);
       expect(favorites.items.contains(number), true);
     });
+    test('An item should be removed', () {
+      var number = 45;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+      favorites.remove(number);
+      expect(favorites.items.contains(number), false);
+    });
   });
 }
